367a324f193f5d0f127691bf6884888baaf75325
javier-gracia-tabuenca-tuni <javier.graciatabuenca@tuni.fi>
2025-10-28 12:36:54 +0200

    Refactor SQL command in insertTable.default to remove database schema reference when dropping existing BigQuery tables

--- R/InsertTable.R
+++ R/InsertTable.R
@@ -493,16 +493,16 @@ insertTable.default <- function(connection,
     }
     if (dropTableIfExists) {
       # bigrquery::bq_table_upload is not dropping tables, so we need to do it manually
-      sql <- "DROP TABLE IF EXISTS @databaseSchema@tableName;"
+      sql <- "DROP TABLE IF EXISTS @tableName;"
       renderTranslateExecuteSql(
         connection = connection,
         sql = sql,
-        databaseSchema = databaseSchema,
         tableName = tableName,
         tempEmulationSchema = tempEmulationSchema,
         progressBar = FALSE,
         reportOverallTime = FALSE
       )
+      dropTableIfExists <- FALSE
     }
   }
   

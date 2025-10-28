b9e280e8d7986e39b5f2c675b6930c2593b738f8
javier-gracia-tabuenca-tuni <javier.graciatabuenca@tuni.fi>
2025-10-28 12:12:36 +0200

    Add functionality to drop existing BigQuery tables before insertion if specified

--- R/InsertTable.R
+++ R/InsertTable.R
@@ -491,6 +491,18 @@ insertTable.default <- function(connection,
       tableName <- SqlRender::translate(sprintf("#%s", tableName), targetDialect = "bigquery", tempEmulationSchema = NULL)
       tempTable <- FALSE
     }
+    if (dropTableIfExists) {
+      # bigrquery::bq_table_upload is not dropping tables, so we need to do it manually
+      sql <- "DROP TABLE IF EXISTS @tableName;"
+      renderTranslateExecuteSql(
+        connection = connection,
+        sql = sql,
+        tableName = tableName,
+        tempEmulationSchema = tempEmulationSchema,
+        progressBar = FALSE,
+        reportOverallTime = FALSE
+      )
+    }
   }
   
   logTrace(sprintf("Inserting %d rows into table '%s' ", nrow(data), tableName))
